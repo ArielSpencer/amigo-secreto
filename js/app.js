7bcf9edd687a0bb1329da2533c5e575524bc9b9e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-05-29 22:48:04 -0300

    Adicionar função de sorteio

--- js/app.js
+++ js/app.js
@@ -1,33 +1,74 @@
-// criar lista
+alert ('Bem-vindo ao sorteio de Amigo Secreto!\n\nRegras:\n1. Adicione os nomes dos participantes um por um.\n2. Não adicione nomes duplicados. Caso haja participantes com o mesmo nome, adicione o sobrenome ou um apelido para diferenciá-los.\n3. É necessário adicionar pelo menos três nomes para realizar o sorteio.\n\nDivirta-se!');
+
 let listaNomes = [];
 
-// adicionar nome digitado na lista e no array
 function adicionar() {
-    let nome = document.getElementById('nome-amigo').value;
+    let nome = document.getElementById('nome-amigo').value.toUpperCase();
+
+    if (nome == '') {
+        alert ('Por favor, digite um nome antes de adicionar.');
+        return;
+    }
+
+    if (listaNomes.includes(nome)) {
+        alert ('Este nome já foi adicionado. Por favor, insira um nome diferente.');
+        limparNome();
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
     listaNomes.push(nome);
 
-    if (lista.textContent == '') {
-        lista.textContent = nome;
-    } else {
-        lista.textContent = lista.textContent + (`, ${nome}`);
-    }
+        if (lista.textContent == '') {
+            lista.textContent = nome;
+        } else {
+            lista.textContent = lista.textContent + (`, ${nome}`);
+        }
     
-    //limpar campo para digitar novo nome
-    limparNome();
-}
+        limparNome();
+    }
 
-// botao sortear = embaralhar os nomes e mostrar lista
 function sortear() {
-    console.log (listaNomes);
+    if (listaNomes.length < 3) {
+        alert ('É necessário adicionar pelo menos três nomes para realizar o sorteio.');
+        return;
+    }
+
+    embaralha(listaNomes);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    if (sorteio.innerHTML != '') {
+        sorteio.innerHTML = '';
+    }
+
+    for (let i = 0; i < listaNomes.length; i++) {
+        if (i == listaNomes.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + listaNomes[i] + ' --> ' + listaNomes[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + listaNomes[i] + ' --> ' + listaNomes[i + 1] + '<br>';
+            }
+    }
 }
 
-// reset
+// fonte função embaralhar: http://cangaceirojavascript.com.br/como-embaralhar-arrays-algoritmo-fisher-yates/
+function embaralha(lista) {
+
+    let indice = lista.length
+    
+    while(indice) {
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
+}
+
 function limparNome() {
     let nome = document.getElementById('nome-amigo');
     nome.value = '';
 }
 
 function reiniciar() {
-
+    listaNomes = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
